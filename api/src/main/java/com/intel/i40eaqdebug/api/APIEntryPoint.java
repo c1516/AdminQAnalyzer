332f63f81141509afdc796c0fe4f154fa03a21e4
Andrew Wen <2060andrew@gmail.com>
2016-01-15 20:12:01 -0800

    Add ability to retrieve log entry queue

--- api/src/main/java/com/intel/i40eaqdebug/api/APIEntryPoint.java
+++ api/src/main/java/com/intel/i40eaqdebug/api/APIEntryPoint.java
@@ -3,9 +3,13 @@ package com.intel.i40eaqdebug.api;
 import com.intel.i40eaqdebug.api.header.CommandStruct;
 import com.intel.i40eaqdebug.api.header.CommandField;
 import com.intel.i40eaqdebug.api.header.Errors;
+import com.intel.i40eaqdebug.api.logs.LogAdapter;
+import com.intel.i40eaqdebug.api.logs.LogEntry;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
 
 /**
  * Public facing entry point, initialization handled internally
@@ -15,7 +19,7 @@ public final class APIEntryPoint {
     private static Errors ERRORS;
     private static Map<Short, String> COMMANDNAMES;
     private static Map<Short, CommandStruct> COMMANDSTRUCTS;
-
+    private static LogAdapter ADAPTER;
     private static CommandStruct UNKNOWN_COMMAND_STRUCT;
 
     static {
@@ -29,11 +33,12 @@ public final class APIEntryPoint {
      * @param errors
      * @param commands
      */
-    static void init(Errors errors, Map<Short, String> names, Map<Short, CommandStruct> commands) {
+    static void init(Errors errors, Map<Short, String> names, Map<Short, CommandStruct> commands, LogAdapter adapter) {
         INIT = true;
         ERRORS = errors;
         COMMANDNAMES = names;
         COMMANDSTRUCTS = commands;
+        ADAPTER = adapter;
     }
 
     public static String getErrorString(byte errFlag) {
@@ -57,6 +62,12 @@ public final class APIEntryPoint {
         return COMMANDSTRUCTS.getOrDefault(opcode, UNKNOWN_COMMAND_STRUCT);
     }
 
+    public static Queue<LogEntry> getCommandLogQueue(File file) {
+        if (!INIT) {
+            throw new IllegalStateException("Attempted to call API methods before initialization");
+        }
+        return ADAPTER.getEntriesSequential(file);
+    }
 
     // Stuff that allows us to return an unknown match without returning null/still be able to display raw bytes
 
