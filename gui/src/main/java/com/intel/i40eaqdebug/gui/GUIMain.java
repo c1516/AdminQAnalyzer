00364a9284d9eee1b5b3d71f31eb715387660520
Robbie Penziol <rpenziol@pdx.edu>
2016-01-16 17:31:19 -0800

    Added Hello World GUI

--- gui/src/main/java/com/intel/i40eaqdebug/gui/GUIMain.java
+++ gui/src/main/java/com/intel/i40eaqdebug/gui/GUIMain.java
@@ -1,7 +1,13 @@
 package com.intel.i40eaqdebug.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+
+import java.io.IOException;
 
 /**
  * Entry point to starting the GUI: API Calls should be called via the APIEntryPoint static methods
@@ -9,11 +15,17 @@ import javafx.stage.Stage;
 
 public class GUIMain extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override public void start(Stage primaryStage) throws IOException {
+        System.out.println(getClass().getResource("/GUIMain.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/GUIMain.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setResizable(true);
+        primaryStage.show();
     }
 
-    @Override public void start(Stage stage) {
-
+    public static void main(String[] args) {
+        launch(args);
     }
 }
