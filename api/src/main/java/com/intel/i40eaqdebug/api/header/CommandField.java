26379a69bc4b5a847a5c6d316bee4169fd3487dd
Andrew Wen <2060andrew@gmail.com>
2016-01-19 16:14:33 -0800

    Document position return behaviour

--- api/src/main/java/com/intel/i40eaqdebug/api/header/CommandField.java
+++ api/src/main/java/com/intel/i40eaqdebug/api/header/CommandField.java
@@ -3,12 +3,18 @@ package com.intel.i40eaqdebug.api.header;
 public interface CommandField {
     // Will merely return as hex, possible expansions are translating said hex into text
     String getValueAsString(byte[] raw);
+    /**
+     * @return The starting byte of this field, inclusive
+     */
     int getStartPos();
+    /**
+     * @return The ending byte of this field, exclusive
+     */
     int getEndPos();
 
     EndianState getEndianness();
 
     enum EndianState {
-        BIG, LITTLE;
+        BIG, LITTLE
     }
 }
