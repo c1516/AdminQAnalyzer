e75fcf537f10d03244eb9a9ff4c888ce025cb3d3
vanquick <vanquick@pdx.edu>
2016-01-27 16:17:14 -0800

    working on details pane

--- gui/src/main/java/com/intel/i40eaqdebug/gui/Controllers/DetailsPaneController.java
+++ gui/src/main/java/com/intel/i40eaqdebug/gui/Controllers/DetailsPaneController.java
@@ -4,6 +4,9 @@ import com.intel.i40eaqdebug.gui.DataModels.TableModel;
 import com.intel.i40eaqdebug.gui.GUIMain;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 
 public class DetailsPaneController {
@@ -26,5 +29,13 @@ public class DetailsPaneController {
 
     @FXML
     public void initialize() {
+        /*Text text1 = new Text("Big italic red text");
+        text1.setFill(Color.RED);
+        text1.setFont(Font.font("Helvetica", FontPosture.ITALIC, 40));
+        Text text2 = new Text(" little bold blue text");
+        text2.setFill(Color.BLUE);
+        text2.setFont(Font.font("Helvetica", FontWeight.BOLD, 10));
+        TextFlow textFlow = new TextFlow(text1, text2);*/
     }
+
 }
