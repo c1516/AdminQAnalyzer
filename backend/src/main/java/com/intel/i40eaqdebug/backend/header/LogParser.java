6b70e8ec3bccc78999fdd4a92dcfeccf2fd599a1
Andrew Wen <2060andrew@gmail.com>
2016-01-29 14:30:26 -0800

    Slightly modify gitignore some more, begin work on discrete reads

--- backend/src/main/java/com/intel/i40eaqdebug/backend/header/LogParser.java
+++ backend/src/main/java/com/intel/i40eaqdebug/backend/header/LogParser.java
@@ -3,10 +3,17 @@ package com.intel.i40eaqdebug.backend.header;
 import com.intel.i40eaqdebug.api.logs.LogEntry;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class LogParser {
-    public LogEntry readDiscreteEntry(BufferedReader reader) {
 
+    public LogEntry readDiscreteEntry(BufferedReader reader) throws IOException {
+        boolean isInBody = true;
+        String line;
+        while ((line = reader.readLine()) != null) {
+
+        }
+        return null;
     }
 
 }
