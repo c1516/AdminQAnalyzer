02a38882049689d9148d1d3f3b76548ad16b7286
Andrew Wen <2060andrew@gmail.com>
2016-01-28 23:58:49 -0800

    Shift focus onto different part of log parsing

--- backend/src/main/java/com/intel/i40eaqdebug/backend/header/LogParser.java
+++ backend/src/main/java/com/intel/i40eaqdebug/backend/header/LogParser.java
@@ -1,12 +1,12 @@
 package com.intel.i40eaqdebug.backend.header;
 
-import java.io.File;
+import com.intel.i40eaqdebug.api.logs.LogEntry;
+
+import java.io.BufferedReader;
 
 public class LogParser {
-    private static String TEMP_FILE_PREFIX = "TEMP_";
-    // Removes junk data from the file line by line
-    public File parseJunk(File file) {
-        File out = new File(TEMP_FILE_PREFIX + file.getName());
+    public LogEntry readDiscreteEntry(BufferedReader reader) {
 
     }
+
 }
