6ffb02b9a74baa8ff65d5df1c61a9d48d335ebc9
Andrew Wen <2060andrew@gmail.com>
2016-02-06 23:50:22 -0800

    More acceptable artifact name

--- api/src/main/java/com/intel/i40eaqdebug/api/logs/LogEntry.java
+++ api/src/main/java/com/intel/i40eaqdebug/api/logs/LogEntry.java
@@ -1,7 +1,9 @@
 package com.intel.i40eaqdebug.api.logs;
 
 public interface LogEntry {
+
     int getStartLine();
+    long getTimeStampMillis();
     byte getErr();
     short getFlags();
     short getOpCode();
