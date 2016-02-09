e568cdb69c4bd78c630e636f853c82947302a58e
Andrey byelogurov <byelogurov@gmail.com>
2016-02-08 21:41:28 -0800

    Added FakeAPIInitiliazer Memorial. May he rest in peace.
    Gone, but not forgotten.

---
+++ gui/src/main/java/com/intel/i40eaqdebug/gui/FakeApiInitilizer_Memorial.java
@@ -0,0 +1,46 @@
+package com.intel.i40eaqdebug.gui;
+
+public class FakeApiInitilizer_Memorial {
+    /*
+                  _  /)
+                  mo / )
+                  |/)\)
+                   /\_
+                   \__|=
+                  (    )
+                  __)(__
+           ______/      \\_____
+          |  _     ___   _    ||
+          | | \     |   | \   ||
+          | |  |    |   |  |  ||
+          | |_/     |   |_/   ||
+          | | \     |   |     ||
+          | |  \    |   |     ||
+          | |   \. _|_. | .   ||
+          |                   ||
+          |  Fake API Init.   ||
+          |Jan 2015 - Feb 2015||
+  *       | *   **    * **    |**      **
+   \))ejm97/.,(//,,..,,\||(,,.,\\,.((//
+*/
+
+    public static void Eulogy() {
+        System.out.println("Here lies Fake API Initializer....");
+        System.out.println("He was a good class, and a better friend");
+        System.out.println("He burned bright and hot, bringing happiness");
+        System.out.println("And laughter to many peoples days,");
+        System.out.println("But alas, like a spark he died to quick,");
+        System.out.println("To Young.");
+        System.out.println("May all that see this grave, remember");
+        System.out.println("Fake API's contribution to the world");
+        System.out.println("And may they condem Andrew W for his ");
+        System.out.println("Harsh actions, that stole Fake API's");
+        System.out.println("existence");
+        System.out.println("----------=[RIP]=---------");
+        System.out.println("---Fake API Initializer---");
+        System.out.println("--01/27/2015  02/08/2016--");
+        System.out.println("--------------------------");
+        System.out.println("Gone, but not forgotten.");
+        System.out.println("");
+    }
+}
